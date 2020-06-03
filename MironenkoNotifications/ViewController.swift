468e77c5e60b5a8752ad2e7b5d42f0c2ff8c4889
Eugene <mir.evgeniy@gmail.com>
2020-06-03 12:30:42 +0500

    Initial Commit

---
+++ MironenkoNotifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MironenkoNotifications
+//
+//  Created by Mironenko Evgeniy on 6/3/20.
+//  Copyright © 2020 Mironenko Evgeniy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
